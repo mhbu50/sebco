ea83a394d4bfa032adb807da70fbb3e15cb2c4bb
mohd200 <bokhamseen.mohammed.m@gmail.com>
2018-05-05 14:37:32 +0300

    custom 5/5

---
+++ sebco/sebco/doctype/annual_employment/annual_employment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Accurate Systems and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Annual Employment', {
+	refresh: function(frm) {
+
+	}
+});
