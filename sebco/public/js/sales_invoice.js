63691e5191d726e37c903095ac2ff3389a48a556
root <mhbu50>
2018-07-03 21:48:09 +0300

    server commite

--- sebco/public/js/sales_invoice.js
+++ sebco/public/js/sales_invoice.js
@@ -2,9 +2,9 @@ frappe.ui.form.on("Sales Invoice", {
   project: function(frm){
     frm.doc.items = [];
     var new_row = frm.add_child("items");
-                new_row.item_code = "Supplying  Man Power";
-                new_row.item_name = "Supplying  Man Power";
-                new_row.description = "Supplying  Man Power";
+                new_row.item_code = "Supplying Man Power";
+                new_row.item_name = "Supplying Man Power";
+                new_row.description = "Supplying Man Power";
                 new_row.conversion_factor = "1";
                 new_row.uom = "Nos";
                 new_row.rate = frm.doc.total_billing_amount;
--- sebco/public/js/sales_order.js
+++ sebco/public/js/sales_order.js
@@ -10,5 +10,27 @@ frappe.ui.form.on("Sales Order", {
       refresh_field("activity_type");
     }
 
+	},
+	customer_po:function(frm){
+		console.log("frm.doc.customer_po",frm.doc.customer_po);
+		if (frm.doc.customer_po){
+	frm.doc.activity_type = [];
+		frappe.call({
+		"method": "frappe.client.get",
+		args: {
+				doctype: "Customer PO",
+				name: frm.doc.customer_po
+		},
+		callback: function (data) {
+			console.log("data",data);
+			$.each(data.message.activity_type, function(index, value) {
+					var new_row = frm.add_child("activity_type");
+					new_row.activity_type = value.activity_type;
+					new_row.billing_rate = value.billing_rate;
+			});
+			refresh_field("activity_type");
+		}
+	});
+}
 	}
 });
