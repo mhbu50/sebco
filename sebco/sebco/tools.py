9f4ed0a21fd81f4d5e54e147cc69e78a2b171f81
mhbu50 <mhbu50@gmail.com>
2018-07-23 15:34:50 +0300

    23/7 tools.py

--- sebco/sebco/tools.py
+++ sebco/sebco/tools.py
@@ -101,17 +101,17 @@ def add_overtime_to_salaryslip(posting_date, start, end):
 			if t.overtime_total:
 				ot = frappe.get_doc({"doctype": "Salary Detail","salary_component": "Over Time", "amount": t.overtime_total})
 				salary.append("earnings", ot)
+				salary.overtime_hours = t.overtime_hours
+				salary.overtime_rate = t.overtime_rate
 				salary.save()
-				ss.overtime_hours = t.overtime_hours
-				ss.overtime_rate = t.overtime_rate
 				frappe.db.sql("update `tabTimesheet Addition` set flag = 1 where name=%s", ot.name)
 			# add Absent
 			if t.absent_total:
 				at = frappe.get_doc({"doctype": "Salary Detail","salary_component": "Absent", "amount": t.absent_total})
 				salary.append("deductions", at)
+				salary.absent_days = t.absent_days
+				salary.absent_rate = t.absent_rate
 				salary.save()
-				ss.absent_days = t.absent_days
-				ss.absent_rate = t.absent_rate
 				frappe.db.sql("update `tabTimesheet Addition` set flag = 1 where name=%s", at.name)
 
 			for e in earning:
